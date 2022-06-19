85c00370e1969d5da1e1b7373c4941af80f8eb80
Leonardo Versiani <leonardovp@gmail.com>
2022-06-19 10:32:59 -0300

    Initial structure for frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import {  Button} from 'antd';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button type='primary'>Botão</Button>
+    </div>
+  );
+}
+
+export default App;
