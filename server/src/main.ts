f18688235b922feb0cea0f5729b57f724c39fc04
Leonardo Versiani <leonardovp@gmail.com>
2022-06-19 15:24:11 -0300

    enableCors

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cors());
-  await app.listen(process.env.PORT || 3000);
+ 
+  try {
+
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    await app.listen(process.env.PORT || 3000);
+
+  } catch (error) {
+    console.log(error);
+  }
+
 }
 bootstrap();
